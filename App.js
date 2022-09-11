21242f1acc847d998c6a4f78215d14a37a47b7aa
PanosKontos <panos96el@gmail.com>
2022-09-11 21:34:15 +0300

    second

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Text Manager</Text>
+      <Text style={styles.text}>Text Manager 101</Text>
       <StatusBar style="auto" />
     </View>
   );
