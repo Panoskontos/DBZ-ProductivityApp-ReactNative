eb95fabcf22dec0ae97afba31b47a4df00814d18
PanosKontos <panos96el@gmail.com>
2022-09-15 22:40:14 +0300

    first

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Images from './comp/Images';
+import { useState } from 'react';
+import React from "react"
 
 
 export default function App() {
+  const [route, setRoute] = useState("Home")
+  const showPages = () =>{
+    
+  }
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Text Manager 101</Text>
+      <Text style={styles.text}>Task Manager 101</Text>
       <StatusBar style="auto" />
 
 
