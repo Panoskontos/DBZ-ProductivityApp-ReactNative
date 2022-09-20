5f1d736ed2da2dfadcae7200f0223acd890e2e9f
PanosKontos <panos96el@gmail.com>
2022-09-21 00:25:59 +0300

    m

--- src/components/StatusCards.js
+++ src/components/StatusCards.js
@@ -9,7 +9,7 @@ export default function StatusCards() {
     const styles = StyleSheet.create({
       statusCards:{
         flexDirection:'row',
-        backgroundColor:'red',
+        // backgroundColor:'red',
         marginBottom:20
 
       },
@@ -18,7 +18,7 @@ export default function StatusCards() {
         flex:1,
         padding:5,
         height:150,
-        backgroundColor:'white',
+        // backgroundColor:'white',
         
       },
 
@@ -36,11 +36,56 @@ export default function StatusCards() {
         textShadowRadius:6.68
 
       },
+
+      card_inprogress:{
+        flex:1,
+        backgroundColor:'#6730bf',
+        borderRadius:10,
+        padding:10,
+        shadowColor:'#7418ff',
+        shadowOffset:{
+          width:0,
+          height:10
+        },
+        shadowOpacity:0.2,
+        textShadowRadius:6.68
+
+      },
+
+      card_deleted:{
+        flex:1,
+        backgroundColor:'#452080',
+        borderRadius:10,
+        padding:10,
+        shadowColor:'#7418ff',
+        shadowOffset:{
+          width:0,
+          height:10
+        },
+        shadowOpacity:0.2,
+        textShadowRadius:6.68
+
+      },
+
+
+
       title:{
+        color:'white',
+        alignSelf:"center"
+
+      },
+      numberContainer:{
+        // backgroundColor:'white',
+        flex:1,
+        alignItems:'center',
+        justifyContent:'center'
 
       },
       number:{
-
+        color:'white',
+        // backgroundColor:'#5611ee',
+        fontSize:35,
+        fontWeight:"100"
       },
        
       });
@@ -49,22 +94,44 @@ export default function StatusCards() {
   return (
     <View style={styles.statusCards}>
       <View style={styles.column}>
+
+
         <View style={styles.card_complete}>
           
           <View>
               <Text style={styles.title}>Completed</Text>
           </View>
+          <View style={styles.numberContainer}>
+              <Text style={styles.number}>100</Text>
+          </View>
+
+        </View>
+
+
+      </View>
+      <View style={styles.column}>
+      <View style={styles.card_inprogress}>
+          
           <View>
+              <Text style={styles.title}>In progress</Text>
+          </View>
+          <View style={styles.numberContainer}>
               <Text style={styles.number}>100</Text>
           </View>
 
         </View>
       </View>
       <View style={styles.column}>
-        <Text>Status Cards</Text>
-      </View>
-      <View style={styles.column}>
-        <Text>Status Cards</Text>
+      <View style={styles.card_deleted}>
+          
+          <View>
+              <Text style={styles.title}>Deleted</Text>
+          </View>
+          <View style={styles.numberContainer}>
+              <Text style={styles.number}>100</Text>
+          </View>
+
+        </View>
       </View>
         
     </View>
